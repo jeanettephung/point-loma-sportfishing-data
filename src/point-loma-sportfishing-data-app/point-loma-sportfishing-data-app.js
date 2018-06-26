1f1ba2e0626e92cc7d810c843143eeea204f988f
jphung@pint.com <jeanettephung@hotmail.com>
2018-06-25 22:05:45 -0700

    add background

--- src/point-loma-sportfishing-data-app/point-loma-sportfishing-data-app.js
+++ src/point-loma-sportfishing-data-app/point-loma-sportfishing-data-app.js
@@ -8,16 +8,46 @@ class PointLomaSportfishingDataApp extends PolymerElement {
   static get template() {
     return html`
       <style>
-        :host {
-          display: block;
+        :host { display:block; height:100vh; }
+        .bg--ocean { background:linear-gradient(rgb(43, 137, 183) 5%, rgb(29, 74, 119) 35%, rgb(19, 12, 58) 90%); height:85%; }
+        .bg--sky { background:linear-gradient(#95ceff, #ccc7c0); height:15%; }
+        .container { margin:0 auto; width:1200px; }
+        .container--grid { align-items:end; display:grid; grid-template-columns:1fr 2fr; grid-template-rows:15vh; }
+        h1 { font-size:1.5rem; margin:0; text-align:center; }
+        .img--boat { left:2rem; position:relative; width:7rem; }
+
+        @media (min-width: 1200px){
+          .bg--ocean { height:75%; }
+          .bg--sky { height:25%; }
+          .container--grid { grid-template-rows:25vh; }
+        }
+
+        @media (min-width: 768px){
+          .container--grid { grid-template-columns:1fr 2fr 1fr; }
+          h1 { font-size:2.5rem; }
+          .img--boat { width:10rem; }
+        }
+
+        @media (max-width: 1200px){
+          .container { width:100%; }
+        }
+
+        @media (max-width: 768px) {
+          h1 { justify-self:start; }
         }
       </style>
-      <div class="bg__sky">
-        <img src="boat">
+
+      <div class="bg--sky">
+        <div class="container  container--grid">
+          <img class="img--boat" src="/assets/img/boat.png" alt="boat">
+          <h1>Point Loma Sportfishing Data</h1>
+        </div>
       </div>
-      <div class="bg__ocean>
-        <jp-carousel></jp-carousel>
-        <calendar-analysis></calendar-analysis>
+      <div class="bg--ocean">
+        <div class="container">
+          <carousel></carousel>
+          <calendar-analysis></calendar-analysis>
+        </div>
       </div>
     `;
   }
